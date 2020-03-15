c42d639ae37c95bb9c0a50ded77c8268aa6f2556
Greg Amato <amatobahn@gmail.com>
2020-03-15 13:30:10 -0500

    Unity Boilerplate Content

---
+++ README.md
@@ -0,0 +1,2 @@
+# skater-xl-unity-boilerplate
+Unity project boilerplate to make maps for SkaterXL
---
+++ sxl_boilerplate/Assets/Editor/EditorTools.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+// using NativeFileBrowser;
+
+public class EditorTools
+{
+    // Build Asset Bundles
+    [MenuItem("SkaterXL/AssetBundles/Build AssetBundles")]
+    static void BuildAssetBundles()
+    {
+        string bundleDirectory = "Assets/AssetBundles";
+        if (!Directory.Exists(bundleDirectory))
+        {
+            Directory.CreateDirectory(bundleDirectory);
+        }
+
+        BuildPipeline.BuildAssetBundles(bundleDirectory, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+    }
+
+    // Capture screenshot for sharing
+    [MenuItem("SkaterXL/Take Screenshot")]
+    static void CaptureScreengrab()
+    {
+        // string path = FileBrowser.SaveFilePanel("Save Screenshot", "", "", ".png");
+		string path = EditorUtility.SaveFilePanel("Save Screenshot", "", "", ".png");
+		Debug.Log(path);
+
+        if (!string.IsNullOrEmpty(path))
+        {
+            UnityEngine.ScreenCapture.CaptureScreenshot(path);
+        }
+    }
+}
