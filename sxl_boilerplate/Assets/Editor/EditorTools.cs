dab83d94ad0a91d7dc712b9b9f343485153ea529
SqueegeeDino <65366947+SqueegeeDino@users.noreply.github.com>
2020-07-27 17:38:17 -0700

    Created new boilerplate project for 1.0
    Wiped the old build and created a new one for 1.0

--- sxl_boilerplate/Assets/Editor/EditorTools.cs
+++ sxl_boilerplate/Assets/Editor/EditorTools.cs
@@ -7,26 +7,13 @@ using UnityEditor;
 
 public class EditorTools
 {
-    // Build Asset Bundles
-    [MenuItem("SkaterXL/AssetBundles/Build AssetBundles")]
-    static void BuildAssetBundles()
-    {
-        string bundleDirectory = "Assets/AssetBundles";
-        if (!Directory.Exists(bundleDirectory))
-        {
-            Directory.CreateDirectory(bundleDirectory);
-        }
-
-        BuildPipeline.BuildAssetBundles(bundleDirectory, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
-    }
-
     // Capture screenshot for sharing
     [MenuItem("SkaterXL/Take Screenshot")]
     static void CaptureScreengrab()
     {
         // string path = FileBrowser.SaveFilePanel("Save Screenshot", "", "", ".png");
-		string path = EditorUtility.SaveFilePanel("Save Screenshot", "", "", ".png");
-		Debug.Log(path);
+        string path = EditorUtility.SaveFilePanel("Save Screenshot", "", "", ".png");
+        Debug.Log(path);
 
         if (!string.IsNullOrEmpty(path))
         {
