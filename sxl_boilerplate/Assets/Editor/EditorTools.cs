f172c96a569efc405d17759d0b47fcf55f4bb9e9
Greg Amato <amatobahn@gmail.com>
2020-07-28 22:33:25 -0500

    File config for sample map

--- sxl_boilerplate/Assets/Editor/EditorTools.cs
+++ sxl_boilerplate/Assets/Editor/EditorTools.cs
@@ -7,13 +7,31 @@ using UnityEditor;
 
 public class EditorTools
 {
+    // Build Asset Bundles
+    [MenuItem("SkaterXL/AssetBundles/Build AssetBundles")]
+    static void BuildAssetBundles()
+    {
+        string bundleDirectory = "Assets/AssetBundles";
+        if (!Directory.Exists(bundleDirectory))
+        {
+            Directory.CreateDirectory(bundleDirectory);
+        }
+
+        BuildPipeline.BuildAssetBundles(bundleDirectory, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+        foreach(string name in AssetDatabase.GetAllAssetBundleNames())
+        {
+            string docFolder = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            File.Copy($"{Application.dataPath}\\AssetBundles\\{name}", $"{docFolder}\\SkaterXL\\Maps\\{name}", true);
+        }
+    }
+
     // Capture screenshot for sharing
     [MenuItem("SkaterXL/Take Screenshot")]
     static void CaptureScreengrab()
     {
         // string path = FileBrowser.SaveFilePanel("Save Screenshot", "", "", ".png");
-        string path = EditorUtility.SaveFilePanel("Save Screenshot", "", "", ".png");
-        Debug.Log(path);
+		string path = EditorUtility.SaveFilePanel("Save Screenshot", "", "", ".png");
+		Debug.Log(path);
 
         if (!string.IsNullOrEmpty(path))
         {
